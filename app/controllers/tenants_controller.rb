c5ef0ffd63294433765a7c7118cbf55b4562687f
Tristan Rubadeau <tristan@rubadeau.com>
2015-02-15 20:32:12 -0700

    new tenant form

--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -8,4 +8,22 @@ class TenantsController < ApplicationController
     @tenant = Tenant.new
   end
 
+  def create
+        @tenant = Tenant.new(tenant_params)
+    if @tenant.save
+      flash[:notice] = "Tenant was successfully created"
+      redirect_to tenants_path(@tenant)
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def tenant_params
+    params.require(:tenant).permit(:name, :user, :lease_start, :lease_end)
+  end
+
+
 end

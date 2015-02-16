a4301e783d82d7a1aaafc14f2f5b6d7bac5dc581
Tristan Rubadeau <tristan@rubadeau.com>
2015-02-15 20:54:11 -0700

    Update and Destroy Tenants

--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -18,6 +18,30 @@ class TenantsController < ApplicationController
     end
   end
 
+    def show
+      @tenant = Tenant.find(params[:id])
+    end
+
+    def edit
+      @tenant = Tenant.find(params[:id])
+    end
+
+    def update
+      @tenant = Tenant.find(params[:id])
+
+      if @tenant.update(tenant_params)
+        flash[:notice] = "Tenant was successfully updated"
+        redirect_to tenant_path(@tenant)
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      tenant = Tenant.find(params[:id])
+      tenant.destroy
+      redirect_to tenants_path
+    end
 
   private
 

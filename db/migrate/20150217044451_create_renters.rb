686d68b92a95ef0cb523e7101af51c5416fd629a
Tristan Rubadeau <tristan@rubadeau.com>
2015-02-16 21:54:56 -0700

    migrated

--- db/migrate/20150217044451_create_renters.rb
+++ db/migrate/20150217044451_create_renters.rb
@@ -2,6 +2,11 @@ class CreateRenters < ActiveRecord::Migration
   def change
     create_table :renters do |t|
 
+      t.string :name
+      t.integer :unit
+      t.datetime :lease_start
+      t.datetime :lease_end
+      
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150213231604) do
+ActiveRecord::Schema.define(version: 20150217044451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "renters", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "unit"
+    t.datetime "lease_start"
+    t.datetime "lease_end"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "tenants", force: :cascade do |t|
     t.string   "name"

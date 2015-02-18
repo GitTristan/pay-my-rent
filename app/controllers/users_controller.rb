32608fd99b32277f58aa9234037707f6c839c05a
Tristan Rubadeau <tristan@rubadeau.com>
2015-02-17 22:14:12 -0700

    UserContr

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,25 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      flash[:notice] = "User was successfully updated"
+      redirect_to users_path
+    else
+      render :edit
+    end
+  end
+
 
   private
 
